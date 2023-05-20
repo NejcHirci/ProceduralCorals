fda2f58098b9df4c31bb41acc4c9624aba3f49c2
Nejc Hirci <nhirci@gmail.com>
2023-05-20 11:19:53 +0200

    Update vite.config.js cause of repo rename

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import glsl from 'vite-plugin-glsl'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/ProceduralCorals/',
+  base: '/procedural-corals/',
   plugins: [glsl()],
 })
